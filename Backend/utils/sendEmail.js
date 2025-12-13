af819ab2e20b67294b978642f327e7b4f6bf2db3
SujalGarg2143 <sujal2143.be23@chitkara.edu.in>
2025-12-14 00:11:14 +0530

    changed mail sender for fast mail sending

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -3,15 +3,16 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail', 
+      host: "smtp.sendgrid.net",
+      port: 587,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: "apikey", 
+        pass: process.env.SENDGRID_API_KEY
       }
     });
 
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+      from: process.env.EMAIL_FROM, 
       to,
       subject,
       text
